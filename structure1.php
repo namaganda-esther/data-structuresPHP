465acfa19d7c296586ddff437148ef2f42844993
Esther Namaganda <45708184+namaganda-esther@users.noreply.github.com>
2021-10-29 23:20:36 +0300

    Add files via upload

---
+++ structure1.php
@@ -0,0 +1,11 @@
+<?php
+echo "ARRAYS <br />";
+
+/* First method to associate create array. */
+$prices = array("matooke" => 15000, "Rice" => 3000, "Beef" => 15000);
+         
+echo "Salary of matooke is ". $prices['matooke'] . "<br />";
+echo "Salary of Rice is ".  $prices['Rice']. "<br />";
+echo "Salary of Beef is ".  $prices['Beef']. "<br />";
+
+?>
